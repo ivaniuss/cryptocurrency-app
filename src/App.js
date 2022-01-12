2388516127be65a6cbc0b5d46d5f31cc14ee71f3
ivaniuss <ivan.emi94@gmail.com>
2022-01-11 22:55:35 -0500

    navbar done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+import Navbar from './components/Navbar';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <Navbar/>
+      </div>
+      <div className='main'>
+        
+      </div>
+      <div className='footer'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
